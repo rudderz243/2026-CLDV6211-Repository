eaac2a55771378445ed27ee0be714c17d4dba55a
Glynn <79942619+rudderz243@users.noreply.github.com>
2026-05-13 16:01:44 +0200

    finished library 2 g1

--- Library2G1/Library2G1/Controllers/CheckoutsController.cs
+++ Library2G1/Library2G1/Controllers/CheckoutsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Library2G1.Models;
+using System.Transactions;
 
 namespace Library2G1.Controllers
 {
@@ -19,11 +20,34 @@ namespace Library2G1.Controllers
         }
 
         // GET: Checkouts
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? searchCustId, int? searchBookId)
         {
-            var library2G1Context = _context.Checkouts.Include(c => c.Book).Include(c => c.Customer);
-            return View(await library2G1Context.ToListAsync());
-        }
+            // create the lists of customers and books for our seaching dropdown lists
+            ViewData["CustList"] = new SelectList(_context.Customers, "CustomerId", "CustomerName", searchCustId);
+            ViewData["BookList"] = new SelectList(_context.Books, "BookId", "BookTitle", searchBookId);
+            // now that we have fed the data into the dropdown lists, we need to check if the user HAS searched
+            bool shouldSearch = searchBookId.HasValue || searchCustId.HasValue;
+            // if the user did not search, return the default values (ALL loans)
+            if (!shouldSearch)
+            {
+                // (existing code)
+                var library2G1Context = _context.Checkouts.Include(c => c.Book).Include(c => c.Customer);
+                return View(await library2G1Context.ToListAsync());
+            }
+            // otherwise, search based on selections
+            // to do that, we first need to create a list that we will manipulate
+            var filterList = _context.Checkouts.Include(c => c.Book).Include(c => c.Customer).AsQueryable();
+            // then we run through each filter
+            if (searchCustId.HasValue) {
+                filterList = filterList.Where(c => c.CustomerId == searchCustId.Value);
+            }
+			if (searchBookId.HasValue)
+			{
+				filterList = filterList.Where(c => c.BookId == searchBookId.Value);
+			}
+            // return the list
+            return View(await filterList.ToListAsync());
+		}
 
         // GET: Checkouts/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -62,6 +86,26 @@ namespace Library2G1.Controllers
         {
             if (ModelState.IsValid)
             {
+                // set the options to talk to the database
+                var transOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.Serializable
+                };
+                // apply those options
+                using (var scope = new TransactionScope(TransactionScopeOption.Required, transOptions,
+                TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    // check for double loans
+                    bool doubleLoaned = await _context.Checkouts.AnyAsync(c => checkout.StartDate <= c.EndDate &&
+                    checkout.EndDate > c.StartDate);
+                    // if loaned out already, error
+                    if (doubleLoaned) 
+                    {
+                        TempData["ErrorMessage"] = "A book cannot be loaned out to two people at the same time.";
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                // else, continue like nothing happened (original code)
                 _context.Add(checkout);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
